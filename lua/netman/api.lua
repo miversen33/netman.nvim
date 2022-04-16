16c96339a7e7fe166c9c9056d534d54155caa743
Mike Iversen <miversen33@gmail.com>
2022-04-16 18:50:06 -0500

    Cleaned formating

--- lua/netman/api.lua
+++ lua/netman/api.lua
@@ -82,7 +82,7 @@ local _read_as_file = function(file)
     local unclaimed_id = M._unclaimed_id_table[origin_path]
     local claim_command = ''
     if unclaimed_id then
-        claim_command = ' | lua require("netman.api"):_claim_buf_details(vim.fn.bufnr(), "' .. M._unclaimed_id_table[origin_path] .. '")' 
+        claim_command = ' | lua require("netman.api"):_claim_buf_details(vim.fn.bufnr(), "' .. M._unclaimed_id_table[origin_path] .. '")'
     end
     log.debug("Processing details: ", {origin_path=origin_path, local_path=local_path, unclaimed_id=unclaimed_id})
     local command = 'read ++edit ' .. local_path .. ' | set nomodified | filetype detect' .. claim_command
