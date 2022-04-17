24f13eac91d687111ac3a2fb2f751c09854d986f
Mike Iversen <miversen33@gmail.com>
2022-04-17 11:20:40 -0500

    Corrected unittest vimrc location in command

--- test/unit-test.sh
+++ test/unit-test.sh
@@ -1,1 +1,1 @@
-$NEOVIM_DIRECTORY/bin/./nvim --headless -u $NEOVIM_PLUGIN_HOME/netman.nvim/test/minimal-testing-config.vim -c "PlenaryBustedDirectory $NEOVIM_PLUGIN_HOME/netman.nvim/test/core/ {minimal_init = '$NEOVIM_PLUGIN_HOME/netman.nvim/test/minimal-config.vim'}"
+$NEOVIM_DIRECTORY/bin/./nvim --headless -u $NEOVIM_PLUGIN_HOME/netman.nvim/test/minimal-testing-config.vim -c "PlenaryBustedDirectory $NEOVIM_PLUGIN_HOME/netman.nvim/test/core/ {minimal_init = '$NEOVIM_PLUGIN_HOME/netman.nvim/test/minimal-testing-config.vim'}"
