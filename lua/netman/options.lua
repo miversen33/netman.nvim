1512b051b8d8367e159d1f826cfe5b6872fccd7a
Mike Iversen <miversen33@gmail.com>
2022-04-17 22:32:54 -0500

    Added more options

--- lua/netman/options.lua
+++ lua/netman/options.lua
@@ -11,6 +11,28 @@ return {
         READ_TYPE = {
             FILE = "FILE"
             ,STREAM = "STREAM"
+            ,EXPLORE = "EXPLORE"
+        }
+        ,ATTRIBUTES = {
+            FILE = "FILE"
+            ,DIRECTORY = "DIRECTORY"
+            ,LINK = "LINK"
+        }
+    }
+    ,protocol = {
+        EXPLORE = 'EXPLORE'
+    }
+    ,explorer = {
+        REQUIRED = {
+            ATTR = 'ATTR'
+            ,NAME = 'NAME'
+            ,UNIQUE_NAME = 'UNIQUE_PATH'
+        }
+        ,OPTIONAL = {
+            FULL_PATH = 'FULL_PATH'
+            ,OWNERSHIP = 'OWNERSHIP'
+            ,PERMISSION = 'PERMISSION'
+            ,TYPE = 'TYPE'
         }
     }
 }
