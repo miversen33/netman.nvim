bc2668ef4ffd96765c35cfa20fcc2cfc5f931b44
Mike Iversen <miversen33@gmail.com>
2022-04-24 18:02:23 -0500

    Cleaned up formatting

--- lua/netman/init.lua
+++ lua/netman/init.lua
@@ -1,13 +1,6 @@
-local _sane_defaults = {
-    log_level = 4
-}
-
-local utils = require('netman.utils')
-
--- utils.adjust_log_level(1)
-
-local api = require('netman.api')
-local log = utils.log
+local api    = require('netman.api')
+local utils  = require("netman.utils")
+local log    = utils.logs
 local notify = utils.notify
 
 local M = {}
@@ -15,6 +8,7 @@ local M = {}
 function M:read(...)
     local files = { f = select("#", ...), ... }
     for _, file in ipairs(files) do
+        notify.warn("Fetching file: " .. file)
         local command = api:read(vim.uri_to_bufnr(file), file)
         if not command then
             log.warn("No command returned for read of " .. file)
@@ -58,13 +52,13 @@ end
 
 function M:close_uri(uri)
     local bufnr = vim.uri_to_bufnr(uri)
-    require("netman.utils").log.debug("Closing Uri: " .. uri .. ' on buffer: ' .. bufnr)
+    log.debug("Closing Uri: " .. uri .. ' on buffer: ' .. bufnr)
     vim.api.nvim_buf_delete(bufnr, {force=false})
 end
 
 function M:init()
     if not M._setup_commands then
-        log.debug("Setting Commands")
+        utils.log.debug("Setting Commands")
         local commands = {
              'command -nargs=1 NmloadProvider lua require("netman.api"):load_provider(<f-args>)'
             ,'command -nargs=? Nmlogs         lua require("netman.api"):dump_info(<f-args>)'
@@ -74,18 +68,12 @@ function M:init()
             ,'command -nargs=1 Nmbrowse       lua require("netman"):read(nil, <f-args>)'
         }
         for _, command in ipairs(commands) do
-            log.debug("Setting Vim Command: " .. command)
+            utils.log.debug("Setting Vim Command: " .. command)
             vim.api.nvim_command(command)
         end
+        M._setup_commands = true
     end
-    M._setup_commands = true
-    -- vim.g.loaded_netman = 1
-    vim.g.loaded_netrwPlugin = 1
-    vim.g.loaded_netrw = 1 -- TODO(Mike) By disabling netrw, we prevent ANY netrw handling of files. This is probably bad, we may want to consider a way to allow some of NetRW to function.
 end
-
-    -- EG, this disables NetRW's local directory handling which is not amazing.
-    -- Alternatively, we build our own internal file handling...?
 
 M:init()
 return M
