0c70ae6ef32675c4f90c6f3b383c484e23f631f7
Mike Iversen <miversen33@gmail.com>
2022-05-06 18:19:17 -0500

    Second failure

--- lua/netman/providers/docker.lua
+++ lua/netman/providers/docker.lua
@@ -1,1 +1,31 @@
- 
+local M = {}
+
+M.protocol_patterns = {}
+M.name = 'docker'
+M.version = 0.1
+
+function M:read(uri, cache)
+
+end
+
+function M:write(buffer_index, uri, cache)
+
+end
+
+function M:delete(uri, cache)
+
+end
+
+function M:get_metadata(requested_metadata)
+
+end
+
+function M:init(config_options)
+
+end
+
+function M:close_connection(buffer_index, uri, cache)
+
+end
+
+return M
