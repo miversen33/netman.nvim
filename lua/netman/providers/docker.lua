473284f5dd4eca52e9ac8af17fe1a8a2b3c74246
Mike Iversen <miversen33@gmail.com>
2022-05-06 18:20:16 -0500

    Post URI Creation

--- lua/netman/providers/docker.lua
+++ lua/netman/providers/docker.lua
@@ -2,6 +2,15 @@ local log = require("netman.utils").log
 local notify = require("netman.utils").notify
 local shell = require("netman.utils").run_shell_command
 local command_flags = require("netman.options").utils.command
+local api_flags = require("netman.options").api
+local string_generator = require("netman.utils").generate_string
+local local_files = require("netman.utils").files_dir
+
+local invalid_permission_glob = '^Got permission denied while trying to connect to the Docker daemon socket at'
+
+local container_pattern     = "^([%a%c%d%s%-%.]*)"
+local path_pattern          = '^([/]+)(.*)$'
+local protocol_pattern      = '^(.*)://'
 
 local M = {}
 
@@ -9,8 +18,60 @@ M.protocol_patterns = {'docker'}
 M.name = 'docker'
 M.version = 0.1
 
+local _parse_uri = function(uri)
+    local details = {
+        base_uri     = uri
+        ,command     = nil
+        ,protocol    = nil
+        ,container   = nil
+        ,path        = nil
+        ,type        = nil
+        ,return_type = nil
+        ,parent      = nil
+    }
+    details.protocol = uri:match(protocol_pattern)
+    uri = uri:gsub(protocol_pattern, '')
+    details.container = uri:match(container_pattern) or ''
+    uri = uri:gsub(container_pattern, '')
+    local path_head, path_body = uri:match(path_pattern)
+    path_body = path_body or ""
+    if (path_head:len() ~= 1 and path_head:len() ~= 3) then
+        notify.error("Error parsing path: Unable to parse path from uri: " .. details.base_uri .. '. Path should begin with either / (Relative) or /// (Absolute) but path begins with ' .. path_head)
+        return {}
+    end
+    if path_head:len() == 1 then
+        details.path = "$HOME/" .. path_body
+    else
+        details.path = "/" .. path_body
+    end
+    if details.path:sub(-1) == '/' then
+        details.type = api_flags.ATTRIBUTES.DIRECTORY
+        details.return_type = api_flags.READ_TYPE.EXPLORE
+    else
+        details.type = api_flags.ATTRIBUTES.FILE
+        details.return_type = api_flags.READ_TYPE.FILE
+        details.unique_name = string_generator(11)
+        details.local_file  = local_files .. details.unique_name
+    end
+    local parent = ''
+    local previous_parent = ''
+    local cur_path = ''
+    -- This is literal 💩 but I dont know a better way of handling this since path globs are awful...
+    for i=1, #details.path do
+        local char = details.path:sub(i,i)
+        cur_path = cur_path .. char
+        if char == '/' then
+            previous_parent = parent
+            parent = parent .. cur_path
+            cur_path = ''
+        end
+    end
+    if cur_path == '' then parent = previous_parent end
+    details.parent = parent
+    return details
+end
 function M:read(uri, cache)
-
+    cache = _parse_uri(uri)
 end
 
 function M:write(buffer_index, uri, cache)
