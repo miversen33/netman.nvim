dc292ba18751db435c6c019c0f072df0fd2fa5b8
Mike Iversen <miversen33@gmail.com>
2022-09-06 03:08:23 -0500

    Removed old stuff

--- test/core-test.lua
+++
@@ -1,6 +0,0 @@
-package.path = "../?.lua;" .. package.path -- Adding netman itself to the import path
-_G._QUIET = true -- This makes bootstrap shut up
-require("lua.netman.tools.bootstrap")
-vim.g.netman_log_level = 0
-require("netman.tools.utils").log.warn("Beginning Unit Tests!")
-require("core.api_spec")
