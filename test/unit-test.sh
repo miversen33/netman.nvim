753f989cd73aea00a260a370a05c64ac5b145f0f
Mike Iversen <miversen33@gmail.com>
2022-09-25 12:25:31 -0500

    Removed core dump note

--- test/unit-test.sh
+++ test/unit-test.sh
@@ -1,8 +1,8 @@
-# For some reason flags (init and write?) cause a core dump
 i=1
 flags=""
 for flag in "$@"
 do
     flags="$flags -t $flag"
 done
-busted -m "../?.lua" --helper lua.netman.tools.bootstrap ./core/ ./behavioral/ ./regression/ $flags
+# TODO: Figure out how to run this regardless of the directory its run from...?
+busted -m "../?.lua"  --keep-going --helper lua.netman.tools.bootstrap ./core/ ./behavioral/ ./regression/ $flags
