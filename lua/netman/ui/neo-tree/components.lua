2569cf76464cce87cc8631adecb8a7e3b0b2ddd2
Mike Iversen <miversen33@gmail.com>
2022-10-23 21:23:50 -0500

    Fixed broken components

--- lua/netman/ui/neo-tree/components.lua
+++ lua/netman/ui/neo-tree/components.lua
@@ -12,18 +12,37 @@
 
 local highlights = require("neo-tree.ui.highlights")
 local common = require("neo-tree.sources.common.components")
+local netman = require("netman.ui.neo-tree")
 
 local M = {}
 
-M.name = function(config, node, state)
-    local highlight = config.highlight or highlights.FILE_NAME
-    return {
-        text = "IS TEST",
-        highlight = highlight,
-    }
+
+M.icon = function(config, node, state)
+    local _icon = common.icon(config, node, state)
+    local internal_node = netman.internal.get_internal_node(node:get_id())
+    if internal_node then
+        if internal_node.type == 'netman_provider' then
+            _icon.text = "  "
+        end
+        if internal_node.icon then
+            _icon.text = string.format("%s ", internal_node.icon)
+        end
+        _icon.highlight = internal_node.hl or _icon.highlight
+    end
+    return _icon
 end
 
---
--- require("netman.tools.utils").log.debug("HELLO!")
+M.state = function(config, node, state)
+    local internal_node = netman.internal.get_internal_node(node:get_id())
+
+    local _state = "  "
+    if internal_node and internal_node.state then
+        _state = string.format("%s ", internal_node.state)
+    end
+    return {
+        text = _state,
+        highlight = nil
+    }
+end
 
 return vim.tbl_deep_extend("force", common, M)
