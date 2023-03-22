3fd1795025cbf26097ba017c6877e38405ac6a08
Mike Iversen <miversen33@gmail.com>
2023-03-21 19:45:22 -0500

    Setup async read function

--- lua/netman/providers/ssh.lua
+++ lua/netman/providers/ssh.lua
@@ -1862,7 +1862,10 @@ function M.internal.read_file(uri, host, callback)
     local _saved_callback = callback
     if callback then
         callback = function(data)
-            _saved_callback(process_handle(data), true)
+            _saved_callback({
+                type = api_flags.READ_TYPE.FILE,
+                data = process_handle(data)
+            })
         end
     end
     local opts = {
