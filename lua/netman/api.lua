f5bdea717dc5c485208118d92a7901dbf0a2f40e
Mike Iversen <miversen33@gmail.com>
2023-04-08 20:27:12 -0500

    Fixed bug with invalid async tag on api proc handle

--- lua/netman/api.lua
+++ lua/netman/api.lua
@@ -903,29 +903,10 @@ function M.internal._read_async(uri, provider, cache, is_connected, output_callb
         end
         protected_callback({data = return_data, type = data.type}, complete)
     end
-    local handle = nil
-    local read_from_handle = function(pipe)
-        return handle and handle.read(pipe)
-    end
-
-    local write_to_handle = function(data)
-        return handle and handle.write(data)
-    end
-
-    local stop_handle = function(_force)
-        dun = true
-        return handle and handle.stop(_force)
-    end
-
-    local return_handle = {
-        async = true,
-        read  = read_from_handle,
-        write = write_to_handle,
-        stop  = stop_handle
-    }
+    local return_handle = M.internal.wrap_shell_handle()
 
     local do_provider_read = function()
-        if dun then
+        if return_handle._stopped then
             -- Figure out what we are returning?
             logger.tracef("Read process for %s was stopped externally. Escaping read", uri)
             opts.callback(nil, true)
@@ -953,14 +934,14 @@ function M.internal._read_async(uri, provider, cache, is_connected, output_callb
             opts.callback(response, true)
             return
         end
-        handle = response.handle
+        return_handle._handle = response.handle
     end
 
     if not is_connected and provider.connect_host_a then
         logger.infof("Attempting provider %s connect to %s", provider.name, uri)
         -- connect to the uri, return a valid return and chain off the
         -- proper exit
-        handle = provider.connect_host_a(
+        local handle = provider.connect_host_a(
             uri,
             cache,
             function(success)
@@ -970,6 +951,11 @@ function M.internal._read_async(uri, provider, cache, is_connected, output_callb
                 do_provider_read()
             end
         )
+        if not handle then
+            logger.warnf("Provider %s did not provide a proper async handle for asynchronous connection event. Removing async connect host", provider.name)
+            provider.connect_host_a = nil
+        end
+        return_handle = M.internal.wrap_shell_handle(handle)
     else
         do_provider_read()
     end
